7c1088b5172be92882dca21b5284a202f8f3227c
Kowalaw <140055290+Kowalaw@users.noreply.github.com>
2023-07-28 03:47:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext("2d") 
+let snake = [{x:150,y:150},{x:140,y:150},{x:130,y:150},{x:120,y:150} ]
+let moveX = 10
+let moveY = 0
+let pomX = 0
+let pomY = 0
+let score = 0
+let dirInterdi = false
+let fin = false
 
-//Style pour définir Rect pour apliqué(position,x,y, taille,hauteur,largeur)
-ctx.fillStyle = "#00AAFF" 
-ctx.fillRect(0,0,canvas.width,canvas.height)
-ctx.strokeStyle = "#000000"
-ctx.strokeRect(0,0,canvas.width,canvas.height)
+function animation(){
+    setTimeout(function(){
+        if(fin){
+            return
+        } else{
+            dirInterdi = false
+            actualisationCanvas()
+            spritePom()
+            moveSnake()
+            creeSnake()
+            animation()
+        }
+    },100)
+}
 
-let snake = [{x:250,y:250},{x:240,y:250},{x:230,y:250},{x:220,y:250} ]
-let vitesseSnake = 10
+animation()
+creeSnake()
+creePom()
+
+function actualisationCanvas(){
+    ctx.fillStyle = "#66AAFF" 
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    ctx.strokeStyle = "#000000"
+    ctx.strokeRect(0,0,canvas.width,canvas.height)
+}
 
 function creeElementDuSnake(elementDuSnake){
-    ctx.fillStyle = "#00FFAA" 
+    ctx.fillStyle = "#009900" 
     ctx.strokeStyle = "#000000"
     ctx.fillRect(elementDuSnake.x,elementDuSnake.y,10,10)
     ctx.strokeRect(elementDuSnake.x,elementDuSnake.y,10,10)
@@ -21,17 +46,109 @@ function creeSnake(){
         creeElementDuSnake(elementDuSnake)
     })
 }
-function movX(){
-    const move = {x:snake[0].x+vitesseSnake, y:snake[0].y}
+function moveSnake(){
+    const move = {x:snake[0].x+moveX, y:snake[0].y+moveY}
     snake.unshift(move)
-    snake.pop()
-}
-function movY(){
-    const move = {x:snake[0].x, y:snake[0].y-vitesseSnake}
-    snake.unshift(move)
-    snake.pop()
+    if(gameOver()){
+        restart()
+        snake.shift(move)
+        fin = true
+        return
+    }
+    const snakeMangePom = snake[0].x === pomX && snake[0].y === pomY
+    if(snakeMangePom){
+        creePom()
+        score ++
+        document.getElementById("score").innerHTML = score
+    } else{
+        snake.pop()
+    }
 }
 
-movX()
-movY()
-creeSnake()
+document.addEventListener('keydown', dirSnake)
+
+function dirSnake(event){
+    if(dirInterdi){return}
+    dirInterdi=true
+
+    const up = 90
+    const left = 81
+    const down =  83
+    const right = 68
+
+    const dirSnake = event.keyCode
+
+    const dirU = moveY=== -10
+    const dirL = moveX=== -10
+    const dirD = moveY=== 10
+    const dirR = moveX=== 10
+
+    if(dirSnake === up && !dirD){
+        moveY = -10
+        moveX = 0
+    }
+    if(dirSnake === left && !dirR) {
+        moveY = 0
+        moveX = -10
+    }
+    if(dirSnake===down && !dirU) {
+        moveY = 10
+        moveX = 0
+    }
+    if(dirSnake===right&&!dirL){
+        moveY = 0
+        moveX = 10
+    }
+}
+
+function positionPom(){
+    return Math.round(Math.random()*(canvas.width-10)/10)*10
+}
+function creePom(){
+    pomX = positionPom()
+    pomY = positionPom()
+
+    snake.forEach(function(partDeSnake){
+        const pomSurSnake = partDeSnake.x == pomX && partDeSnake.y == pomY
+        if(pomSurSnake){
+            creePom()
+        }
+    })
+}
+function spritePom(){
+    ctx.fillStyle = '#FF0000'
+    ctx.strokeStyle = "#000000"
+    ctx.beginPath()
+    ctx.arc(pomX+5,pomY+5,5,0,2*Math.PI)
+    ctx.fill()
+    ctx.stroke()
+}
+
+function gameOver(){
+    let corpDeSnake = snake.slice(1,-1)
+    let mortLaQueue = false
+    corpDeSnake.forEach(corp=>{
+        if(snake[0].x === corp.x &&snake[0].y===corp.y){
+            mortLaQueue = true
+        }
+    })
+    const murG = snake[0].x < -1
+    const murH = snake[0].y < -1
+    const murD = snake[0].x > canvas.width-10
+    const murB = snake[0].y > canvas.width-10
+    let gameOver = false
+    if(mortLaQueue||murG||murH||murD||murB){
+        gameOver=true
+    }
+    return gameOver
+}
+function restart(){
+    const restart = document.getElementById('prestart')
+    restart.style.display = "block"
+
+    document.addEventListener("keydown",(event)=>{
+        if(event.keyCode===32){
+            document.location.reload(true)
+        }
+    })
+}
